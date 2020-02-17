653f0704a31f0045b87b39bf17132354c8ba9dd2
Charles <HUCharles@foxmail.com>
2020-02-17 19:09:37 +0800

    <😂> 1.去除机器学习 2.增加MongoDB学习

--- 机器学习/手写数字识别/munist.py
+++
@@ -1,2 +0,0 @@
-from tensorflow.examples.tutorials.mnist import input_data
-MNIST = input_data.read_data_sets("/Mnist", one_hot=True)
--- 机器学习/输出HelloWorld/code.py
+++
@@ -1,4 +0,0 @@
-import tensorflow as tf
-hello = tf.constant("Hello,tensorflow!")
-sess = tf.Session()
-print(sess.run(hello))
